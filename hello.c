717c56370e1552fac5009f7fe58211a12002384a
jonwongcs <thinkstation@hotmail.com>
2018-11-26 05:28:09 +0000

    add hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+ * File: hello.c
+ * -------------
+ * Exercise 1
+ * This program prints "hello, world" on screen.
+ */
+
+ #include <stdio.h>
+
+ int main()
+ {
+     printf("hello github");
+     return 0;
+ }
